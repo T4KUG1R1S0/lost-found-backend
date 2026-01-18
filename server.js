735b8efb1ee8b0cc5727528653c62e70659a80fa
T4KUG1R1S0 <Ardayanto08@gmail.com>
2026-01-18 23:04:48 +0700

    initial backend lost and found

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const DATA_FILE = "./data/items.json";
+
+// ambil semua data
+app.get("/items", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(DATA_FILE));
+  res.json(data);
+});
+
+// tambah barang hilang
+app.post("/items", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(DATA_FILE));
+  const newItem = {
+    id: Date.now(),
+    name: req.body.name,
+    location: req.body.location,
+    description: req.body.description,
+    status: "hilang"
+  };
+  data.push(newItem);
+  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+  res.json({ message: "Data berhasil ditambahkan" });
+});
+
+// update status (admin)
+app.put("/items/:id", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(DATA_FILE));
+  const item = data.find(i => i.id == req.params.id);
+  item.status = req.body.status;
+  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+  res.json({ message: "Status diubah" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running di http://localhost:3000");
+});
